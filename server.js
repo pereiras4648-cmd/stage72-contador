0b1715d997b52816cb0e50a208ec556d31430d7b
pereiras4648-cmd <pereiras4648@gmail.com>
2026-08-27 23:02:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
   res.setHeader(
     "Access-Control-Allow-Headers",
-   "Content-Type, Accept, Authorization, X-Stage72-Admin-Key"
+  "Content-Type, Accept, Authorization"
   );
 
   if (req.method === "OPTIONS") {
